4d191d4ff2f880c2a406ce7928872092a3c07092
Jason Mulligan <jason.mulligan@avoidwork.com>
2014-04-01 13:51:21 -0400

    Getting the auth UI looking decent

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,27 @@
 ( function ( document, window, $ ) {
-	var $h1 = $("h1");
+	var $auth   = $( "#authArea" ),
+	    $form   = $( "#authForm" ),
+	    NOTHASH = /.*#/;
 
-	debugger;
+	$auth.hide();
+
+	// Click listeners on auth buttons parentNode
+	$( ".profileOptions" ).on( "click", function ( ev ) {
+		var $ev     = $( ev ),
+		    $target = $( ev.target );
+
+		$ev.stop();
+		$auth.hide();
+
+		$.get( $target[0].href.replace( NOTHASH, "" ) + "/" ).then( function ( arg ) {
+			$auth.html( arg );
+			$auth.show();
+		}, function ( e ) {
+			$auth.html( "Something went wrong" );
+			$auth.show();
+		} );
+	} );
+
+	// Clearing hash on load
+	document.location.hash = "";
 } )( document, window, jQuery );

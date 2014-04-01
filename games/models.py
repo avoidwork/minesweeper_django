3fb31b564213e765f5e0414c8780dcc5ce2e4675
Jason Mulligan <jason.mulligan@avoidwork.com>
2014-04-01 10:12:57 -0400

    Creating `games` model

--- games/models.py
+++ games/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Games(models.Model):
+    max_x = models.PositiveSmallIntegerField()
+    max_y = models.PositiveSmallIntegerField()
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+
+class Moves(models.Model):
+    game = models.ForeignKey(Games)
+    x = models.PositiveSmallIntegerField()
+    y = models.PositiveSmallIntegerField()
+    move_date = models.DateTimeField(auto_now_add=True)

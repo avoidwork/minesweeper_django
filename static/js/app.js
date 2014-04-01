105e49f73f64015d62b8b223e8a28dddf7c203a2
Jason Mulligan <jason.mulligan@avoidwork.com>
2014-04-01 15:22:35 -0400

    Passing `X-CSRFToken` header for authentication

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,59 @@
 ( function ( document, window, $ ) {
 	var $auth   = $( "#authArea" ),
 	    $form   = $( "#authForm" ),
+	    $csrf   = $.cookie( "csrftoken" ),
 	    NOTHASH = /.*#/;
 
 	$auth.hide();
 
+	// Prepares a form submission
+	function submit ( type ) {
+		var data = {};
+
+		$form.find( "input" ).each( function ( idx, i ) {
+			data[i.id] = i.value;
+		} );
+
+		return $.ajax( {
+			type    : "POST",
+			url     : type + "/",
+			data    : data,
+			headers : {"X-CSRFToken": $csrf}
+		} );
+	}
+
+	// Ready to start/resume a game
+	function ready ( arg ) {
+		debugger;
+	}
+
 	// Click listeners on auth buttons parentNode
 	$( ".profileOptions" ).on( "click", function ( ev ) {
 		var $ev     = $( ev ),
-		    $target = $( ev.target );
+		    $target = $( ev.target ),
+		    $type   = $target[0].href.replace( NOTHASH, "" );
 
 		$ev.stop();
 		$auth.hide();
 
-		$.get( $target[0].href.replace( NOTHASH, "" ) + "/" ).then( function ( arg ) {
+		$.get( $type + "/" ).then( function ( arg ) {
 			$auth.html( arg );
 			$auth.show();
+
+			$auth.find( "a[href='#submit']" ).on( "click", function ( ev ) {
+				ev.preventDefault();
+				$( ev ).stop();
+
+				submit( $type ).then( ready, function ( e ) {
+					$( ".error" ).html( "Something went wrong" );
+				} );
+			} );
+
+			$auth.find( "a[href='#reset']" ).on( "click", function ( ev ) {
+				ev.preventDefault();
+				$( ev ).stop();
+				$form[0].reset();
+			} );
 		}, function ( e ) {
 			$auth.html( "Something went wrong" );
 			$auth.show();

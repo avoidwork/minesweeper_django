9a2fc27cdae98051abafa417429e368d3aa8c6af
Jason Mulligan <jason.mulligan@avoidwork.com>
2014-04-02 06:34:40 -0400

    Setting initial list item in `response.clear` if no `mine` is found, needs to recursively "move" on all related blocks now

--- games/views.py
+++ games/views.py
@@ -35,15 +35,16 @@ def move(request, game_id):
         y = request.POST['y']
 
         game = Game.objects.get(pk=game_id)
-        mine = Mine.objects.filter(game=game, x=x, y=y)
+        mine = Mine.objects.filter(game=game, x=x, y=y).count()
 
         response_data = {}
         response_data['clear'] = list()
         response_data['epoch'] = None
 
-        if mine is None:
+        if mine == 0:
             move = Move(game=game, x=x, y=y)
             move.save()
+            response_data['clear'].insert(0, {"x": x, "y": y})
             response_data['result'] = 'success'
             response_data['epoch'] = int(time.mktime(move.move_date.timetuple())*1000)/1000
         else:
